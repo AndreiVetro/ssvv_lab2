c7d509549022ad280d1d4bf284ae16d839045f7a
mazaremoale <galaxysandrei@gmail.com>
2019-03-22 22:40:35 +0200

    Test commit

--- src/test/java/lab/StudentTests.java
+++ src/test/java/lab/StudentTests.java
@@ -1,5 +1,63 @@
 package lab;
 
+import lab.src.domain.Student;
+import lab.src.repository.NotaXMLRepo;
+import lab.src.repository.StudentXMLRepo;
+import lab.src.repository.TemaXMLRepo;
+import lab.src.service.Service;
+import lab.src.validation.NotaValidator;
+import lab.src.validation.StudentValidator;
+import lab.src.validation.TemaValidator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StudentTests
 {
+    private Student student;
+    private Service service;
+    private StudentXMLRepo studentXMLRepository;
+    private TemaXMLRepo temaXMLRepository;
+    private NotaXMLRepo notaXMLRepository;
+    private StudentValidator studentValidator;
+    private TemaValidator temaValidator;
+    private NotaValidator notaValidator;
+
+    @Before
+    public void setup()
+    {
+        String filenameStudent = "src/test/fisiere_test/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        student = new Student("ssie4233", "Sanziana Sorea", 936, "sanzi@gmail.com");
+        studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        temaXMLRepository = new TemaXMLRepo(filenameTema);
+        notaXMLRepository = new NotaXMLRepo(filenameNota);
+        temaValidator = new TemaValidator();
+        studentValidator = new StudentValidator();
+        notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+
+    @Test
+    public void addStudentTestExpectNull()
+    {
+        Student result = service.addStudent(student);
+        assert result == null;
+    }
+
+    @Test
+    public void addStudentTestExpectStudent()
+    {
+        service.addStudent(student);
+        Student result = service.addStudent(student);
+        assert result.equals(student);
+    }
+
+    @After
+    public void teardown()
+    {
+        service.deleteStudent(student.getID());
+    }
 }
